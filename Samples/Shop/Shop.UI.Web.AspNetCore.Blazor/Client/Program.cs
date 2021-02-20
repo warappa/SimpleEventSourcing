d3854d5e5e7586bcaf35378b2e6f58e0a4ee41d8
warappa <spoiltnr1@gmx.at>
2021-02-20 23:51:33 +0100

    Adding MudBlazor, updating Blazor articles page, adding article detail page

--- Samples/Shop/Shop.UI.Web.AspNetCore.Blazor/Client/Program.cs
+++ Samples/Shop/Shop.UI.Web.AspNetCore.Blazor/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -18,6 +19,7 @@ namespace Shop.UI.Web.AspNetCore.Blazor.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
