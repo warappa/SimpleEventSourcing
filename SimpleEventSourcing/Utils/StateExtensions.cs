9e0121ace02ab57149552bef19b887784e991765
warappa <spoiltnr1@gmx.at>
2021-05-13 17:05:10 +0200

    Support VoidTaskResult

--- SimpleEventSourcing/Utils/StateExtensions.cs
+++ SimpleEventSourcing/Utils/StateExtensions.cs
@@ -17,17 +17,40 @@ namespace SimpleEventSourcing.Utils
                 return null;
             }
 
-            if (result is Task)
+            if(result is TState res)
+            {
+                return res;
+            }
+
+            var wasTask = false;
+
+            if (result is Task task)
+            {
+                task.Wait();
+                wasTask = true;
+            }
+            else if (result is ValueTask valueTask)
+            {
+                valueTask.GetAwaiter().GetResult();
+
+                wasTask = true;
+            }
+
+            if (wasTask)
             {
                 var type = result.GetType();
 
                 if (!cachedResultProperties.TryGetValue(type, out var property))
                 {
                     property = type.GetRuntimeProperty("Result");
+                    if (property.PropertyType.Name == "VoidTaskResult")
+                    {
+                        property = null;
+                    }
                     cachedResultProperties[type] = property;
                 }
 
-                return property.GetValue(result) as TState;
+                result = property?.GetValue(result);
             }
 
             return (TState)result;
