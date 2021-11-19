2f826291d6106d4d855b88a8f995480e02c02496
warappa <spoiltnr1@gmx.at>
2021-11-19 21:39:48 +0100

    STJ: Fix extension method name

--- SimpleEventSourcing.System.Text.Json/ServiceCollectionExtensions.cs
+++ SimpleEventSourcing.System.Text.Json/ServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static ISimpleEventSourcingBuilder AddNewtonsoftJson(this ISimpleEventSourcingBuilder builder)
+        public static ISimpleEventSourcingBuilder AddSystemTextJson(this ISimpleEventSourcingBuilder builder)
         {
             var binder = new VersionedBinder();
             builder.Services.Replace(new ServiceDescriptor(typeof(ISerializer), new SystemTextJsonSerializer(binder)));
